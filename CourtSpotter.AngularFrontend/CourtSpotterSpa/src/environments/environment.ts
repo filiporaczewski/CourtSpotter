f4b22abcc8c8578aeea2f10d47e3eee8a6c20393
filiporaczewski <filip.oraczewski@gmail.com>
2025-07-15 15:03:54 +0200

    Update environment redirect URIs to use 'www' subdomain

--- CourtSpotter.AngularFrontend/CourtSpotterSpa/src/environments/environment.ts
+++ CourtSpotter.AngularFrontend/CourtSpotterSpa/src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
   auth: {
     clientId: '6813eb99-ff9f-47be-a393-90a3e0cd0284',
     authority: 'https://login.microsoftonline.com/eb8939b1-9b93-4256-9aea-c2d994553fb5',
-    redirectUri: 'https://courtspotter.org/admin',
-    postLogoutRedirectUri: 'https://courtspotter.org'
+    redirectUri: 'https://www.courtspotter.org/admin',
+    postLogoutRedirectUri: 'https://www.courtspotter.org'
   },
   api_scopes: ['api://3daf7ebf-703a-40a3-a0d9-83fce29fe2fa/padelclubs.create']
 };
